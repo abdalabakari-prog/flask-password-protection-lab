5bc926076442a704550efda433d70749fa10f451
abdalabakari-prog <abdala.bakari@student.moringaschool.com>
2026-03-30 11:53:56 +0300

    Initial commit

--- server/app.py
+++ server/app.py
@@ -7,15 +7,48 @@ from config import app, db, api
 from models import User, UserSchema
 
 class ClearSession(Resource):
-
     def delete(self):
-    
         session['page_views'] = None
         session['user_id'] = None
-
         return {}, 204
 
-api.add_resource(ClearSession, '/clear', endpoint='clear')
+class Signup(Resource):
+    def post(self):
+        data = request.get_json()
+        user = User(username=data['username'])
+        user.password_hash = data['password']
+        db.session.add(user)
+        db.session.commit()
+        session['user_id'] = user.id
+        return UserSchema().dump(user), 201
+
+class CheckSession(Resource):
+    def get(self):
+        user_id = session.get('user_id')
+        if user_id:
+            user = User.query.get(user_id)
+            return UserSchema().dump(user), 200
+        return {}, 204
+
+class Login(Resource):
+    def post(self):
+        data = request.get_json()
+        user = User.query.filter_by(username=data['username']).first()
+        if user and user.authenticate(data['password']):
+            session['user_id'] = user.id
+            return UserSchema().dump(user), 200
+        return {'message': 'Invalid credentials'}, 401
+
+class Logout(Resource):
+    def delete(self):
+        session['user_id'] = None
+        return {}, 204
+
+api.add_resource(ClearSession, '/clear')
+api.add_resource(Signup, '/signup')
+api.add_resource(CheckSession, '/check_session')
+api.add_resource(Login, '/login')
+api.add_resource(Logout, '/logout')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
